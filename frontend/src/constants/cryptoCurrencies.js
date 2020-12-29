4e9c9bcb55ca5fc8bf32b86f325cd15b6d777fd4
fmelp <melpignanof@gmail.com>
2020-12-29 14:57:57 +0100

    pair icons

--- frontend/src/constants/cryptoCurrencies.js
+++ frontend/src/constants/cryptoCurrencies.js
@@ -1,9 +1,11 @@
 import React from 'react';
 import { ReactComponent as EthereumIcon } from '../assets/images/crypto/ethereum.svg';
 import { ReactComponent as KadenaIcon } from '../assets/images/crypto/kadena-logo.svg';
+import { ReactComponent as AbcIcon } from '../assets/images/crypto/abc-logo.svg';
+import { ReactComponent as XyzIcon } from '../assets/images/crypto/xyz-logo.svg';
 
 export default {
   KDA: { name: 'coin', code: 'KDA', icon: <KadenaIcon /> },
-  ABC: { name: 'kswap.abc', code: 'ABC', icon: <KadenaIcon /> },
-  XYZ: { name: 'kswap.xyz', code: 'XYZ', icon: <KadenaIcon /> }
+  ABC: { name: 'kswap.abc', code: 'ABC', icon: <AbcIcon /> },
+  XYZ: { name: 'kswap.xyz', code: 'XYZ', icon: <XyzIcon /> }
 };
--- frontend/src/constants/pairTokens.js
+++ frontend/src/constants/pairTokens.js
@@ -1,24 +1,26 @@
 import React from 'react';
 import { ReactComponent as EthereumIcon } from '../assets/images/crypto/ethereum.svg';
 import { ReactComponent as KadenaIcon } from '../assets/images/crypto/kadena-logo.svg';
+import { ReactComponent as AbcIcon } from '../assets/images/crypto/abc-logo.svg';
+import { ReactComponent as XyzIcon } from '../assets/images/crypto/xyz-logo.svg';
 
 export default {
   "coin:kswap.abc": {
     name: "coin:kswap.abc",
     pairAccount: "",
     token0:  { name: 'coin', code: 'KDA', icon: <KadenaIcon /> },
-    token1: { name: 'kswap.abc', code: 'ABC', icon: <KadenaIcon />}
+    token1: { name: 'kswap.abc', code: 'ABC', icon: <AbcIcon style={{width: 16, height: 16}}/>}
   },
   "coin:kswap.xyz": {
     name: "coin:kswap.xyz",
     pairAccount: "",
     token0:  { name: 'coin', code: 'KDA', icon: <KadenaIcon /> },
-    token1: { name: 'kswap.xyz', code: 'XYZ', icon: <KadenaIcon />}
+    token1: { name: 'kswap.xyz', code: 'XYZ', icon: <XyzIcon style={{width: 16, height: 16}}/>}
   },
   "kswap.abc:kswap.xyz": {
     name: "kswap.abc:kswap.xyz",
     pairAccount: "",
-    token0:  { name: 'kswap.abc', code: 'ABC', icon: <KadenaIcon /> },
-    token1: { name: 'kswap.xyz', code: 'XYZ', icon: <KadenaIcon />}
+    token0:  { name: 'kswap.abc', code: 'ABC', icon: <AbcIcon style={{width: 16, height: 16}}/> },
+    token1: { name: 'kswap.xyz', code: 'XYZ', icon: <XyzIcon style={{width: 16, height: 16}}/>}
   }
 }
