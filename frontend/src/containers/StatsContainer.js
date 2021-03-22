8bfa9398c3f9b31c33e45dd5f0684c43614a1b56
ggobugi27 <ggobugi27@github.com>
2021-03-23 02:38:20 +0900

    re-shape stats with a table

--- frontend/src/containers/StatsContainer.js
+++ frontend/src/containers/StatsContainer.js
@@ -2,7 +2,7 @@ import React from 'react';
 import styled from 'styled-components/macro';
 import FormContainer from '../components/shared/FormContainer';
 import { ReactComponent as KadenaLogo } from '../assets/images/crypto/kadena-logo.svg';
-import { Dimmer, Loader } from 'semantic-ui-react'
+import { Dimmer, Loader, Table, Menu, Label } from 'semantic-ui-react'
 import { PactContext } from '../contexts/PactContext';
 import {reduceBalance, extractDecimal} from '../utils/reduceBalance';
 import { ReactComponent as CloseIcon } from '../assets/images/shared/cross.svg';
@@ -33,13 +33,14 @@ const IconsContainer = styled.div`
   justify-content: space-between;
   align-items: center;
   margin-right: 21px;
+  width: 80px;
 
   svg:first-child {
     z-index: 2;
   }
 
-  svg:last-child {
-    margin-left: 1px;
+  div:last-child {
+    margin-left: 5px;
   }
 `;
 
@@ -53,37 +54,46 @@ const StatsContainer = ({ data }) => {
 
   return (
     <Container>
-      <FormContainer title="pool stats"  containerStyle={{ maxWidth: 500 }}>
-        <TitlesContainer>
-          <span style={{ width: 120 }}>Name</span>
-          <span style={{ width: 150 }}>Total Reserve - token0 </span>
-          <span style={{ width: 150 }}>Total Reserve - token1</span>
-          <span style={{ width: 80 }}>Rate</span>
-        </TitlesContainer>
-        {pact.pairList[0]
-        ?
-        Object.values(pact.pairList).map(pair => (
-          pair&&pair.reserves ?
-          <Row key={pair.name}>
-            <div style={{ marginLeft: 0,  flex: 0.2 }}>
-              <IconsContainer style={{ width:30 }}>
-                {pact.tokenData[pair.token0].icon}
-                {pact.tokenData[pair.token1].icon}
-              </IconsContainer>
-            </div>
-            <div style={{ marginLeft: -10, width:30 }}>{`${pair.token0}/${pair.token1}`}</div>
-            <div style={{marginLeft: 45, width:150 }}>{reduceBalance(pair.reserves[0])}</div>
-            <div style={{marginLeft: 10, width:150 }}>{reduceBalance(pair.reserves[1])}</div>
-            <div style={{marginLeft: 5}}>{`${reduceBalance(extractDecimal(pair.reserves[0])/extractDecimal(pair.reserves[1]))} ${pair.token0}/${pair.token1}`}</div>
-          </Row>
-          :""
-        ))
-        :
-        <Dimmer active inverted>
-          <Loader>Loading</Loader>
-        </Dimmer>
-      }
-      </FormContainer>
+      <FormContainer title="pool stats"  containerStyle={{ maxWidth: 650 }}>
+
+        <Table celled basic='very'>
+         <Table.Header>
+           <Table.Row style={{fontFamily: "neue-bold"}}>
+             <Table.HeaderCell textAlign='center'>Name</Table.HeaderCell>
+             <Table.HeaderCell textAlign='center'>Total Reserve - <br/>  token0</Table.HeaderCell>
+             <Table.HeaderCell textAlign='center'>Total Reserve - <br/>  token1</Table.HeaderCell>
+             <Table.HeaderCell textAlign='center'>Rate</Table.HeaderCell>
+           </Table.Row>
+         </Table.Header>
+         {
+         pact.pairList[0]
+         ?
+         Object.values(pact.pairList).map(pair => (
+           pair&&pair.reserves
+           ?
+           <Table.Body>
+             <Table.Row key={pair.name}>
+               <Table.Cell>
+                 <IconsContainer>
+                   {pact.tokenData[pair.token0].icon}
+                   {pact.tokenData[pair.token1].icon}
+                   <div>{`${pair.token0}/${pair.token1}`}</div>
+                 </IconsContainer>
+               </Table.Cell>
+               <Table.Cell>{reduceBalance(pair.reserves[0])}</Table.Cell>
+               <Table.Cell>{reduceBalance(pair.reserves[1])}</Table.Cell>
+               <Table.Cell>{`${reduceBalance(extractDecimal(pair.reserves[0])/extractDecimal(pair.reserves[1]))} ${pair.token0}/${pair.token1}`}</Table.Cell>
+             </Table.Row>
+           </Table.Body>
+           :""
+         ))
+         :
+         <Dimmer active inverted>
+           <Loader>Loading</Loader>
+         </Dimmer>
+         }
+       </Table>
+     </FormContainer>
     </Container>
   );
 };
