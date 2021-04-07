a043b62be2d7ff6b1cbfdc1a450a1e49683e0438
Kate Hee Kyun Yun <31594593+ggobugi27@users.noreply.github.com>
2021-04-07 22:46:11 +0900

    Redeem guide (#138)
    * fix a tag to navlink
    
    Co-authored-by: ggobugi27 <ggobugi27@github.com>

--- frontend/src/modals/RedeemGuide.js
+++ frontend/src/modals/RedeemGuide.js
@@ -1,5 +1,7 @@
 import React from 'react'
 import { Button, Header, Image, Modal, Label, Divider } from 'semantic-ui-react'
+import { NavLink, useHistory } from 'react-router-dom';
+import { ROUTE_INDEX, ROUTE_POOL, ROUTE_SWAP, ROUTE_KPY_RED } from '../router/routes';
 
 function ModalExampleModal() {
   const [open, setOpen] = React.useState(true)
@@ -11,21 +13,21 @@ function ModalExampleModal() {
       open={open}
     >
       <Modal.Header>
-        <p>Quick Guide on KPenny Redeem<Label as='a' color='red' tag> Upcoming </Label></p>
+        <p>Quick Guide on KPenny Redeem  <Label as='a' color='red' tag> Upcoming </Label></p>
       </Modal.Header>
       <Modal.Content>
         <h4> To do BEFORE 4/21/2021 00:00 UTC</h4>
         <Modal.Description>
           <p>
-            1. Remove your liquidity pair (ABC/XYZ, ABC/KPY, KPY/XYZ) from the <a href="https://kadenaswap.chainweb.com/pool">liquidity pool</a>.
+            1. Remove your liquidity pair (ABC/XYZ, ABC/KPY, KPY/XYZ) from the <NavLink to={ROUTE_POOL} onClick={() => setOpen(false)}>liquidity pool</NavLink>.
           </p>
           <p>
-            2. Swap your ABC, and XYZ tokens to KPY <a href="https://kadenaswap.chainweb.com/swap"> here</a>!
+            2. Swap your ABC, and XYZ tokens to KPY <NavLink to={ROUTE_SWAP} onClick={() => setOpen(false)}> here</NavLink>!
           </p>
           <Divider/>
           <h4> To do AFTER 4/21/2021 00:00 UTC </h4>
           <p>
-            3. Redeem your KPY to KDA <a href="https://kadenaswap.chainweb.com/kpenny-redeem"> here</a>!
+            3. Redeem your KPY to KDA <NavLink to={ROUTE_KPY_RED} onClick={() => setOpen(false)}> here</NavLink>!
           </p>
         </Modal.Description>
       </Modal.Content>
