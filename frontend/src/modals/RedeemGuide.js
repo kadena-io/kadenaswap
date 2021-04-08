f3e909655eb4d8ef63dab466e6c130e123e44850
ggobugi27 <ggobugi27@github.com>
2021-04-08 13:00:42 +0900

    undo date change

--- frontend/src/modals/RedeemGuide.js
+++ frontend/src/modals/RedeemGuide.js
@@ -16,7 +16,7 @@ function ModalExampleModal() {
         <p>Quick Guide on KPenny Redeem  <Label as='a' color='red' tag> Upcoming </Label></p>
       </Modal.Header>
       <Modal.Content>
-        <h4> To do BEFORE 4/21/2021 00:00 UTC</h4>
+        <h4> To do BEFORE 4/15/2021 00:00 UTC</h4>
         <Modal.Description>
           <p>
             1. Remove your liquidity pair (ABC/XYZ, ABC/KPY, KPY/XYZ) from the <NavLink to={ROUTE_POOL} onClick={() => setOpen(false)}>liquidity pool</NavLink>.
@@ -25,7 +25,7 @@ function ModalExampleModal() {
             2. Swap your ABC, and XYZ tokens to KPY <NavLink to={ROUTE_SWAP} onClick={() => setOpen(false)}> here</NavLink>!
           </p>
           <Divider/>
-          <h4> To do AFTER 4/21/2021 00:00 UTC </h4>
+          <h4> To do AFTER 4/15/2021 00:00 UTC </h4>
           <p>
             3. Redeem your KPY to KDA <NavLink to={ROUTE_KPY_RED} onClick={() => setOpen(false)}> here</NavLink>!
           </p>
