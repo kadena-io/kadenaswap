54bbbff9c190754b3f0cf8c50a85527540a2575e
Kate Hee Kyun Yun <31594593+ggobugi27@users.noreply.github.com>
2021-07-06 19:07:42 -0400

    Migrate relay (#160)
    * delete relay-fe
    
    Co-authored-by: Hee kyun Yun <ggobugi27@github.com>

--- relay-fe/src/wallet/components/alerts/walletSigError.js
+++
@@ -1,9 +0,0 @@
-import swal from '@sweetalert/with-react'
-import './alert.css'
-
-export default async () => {
-  return await swal({
-    text: "You cancelled the transaction or did not sign it correctly. Please make sure you sign with the keys of the account linked in Kadenaswap",
-    title: "Wallet Signing Failure"
-  })
-};
